1e26996c354ac94b8834603e7c5260c00da7a363
Mazen <mazenghaleb@outlook.com>
2024-05-29 05:23:18 +0300

    Implemented Lazy Loading and Preloading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -20,9 +16,6 @@ import { HeaderComponent } from './header/header.component';
     HttpClientModule,
     CoreModule,
     SharedModule,
-    AuthModule,
-    RecipesModule,
-    ShoppingListModule,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { SharedModule } from '../shared/shared.module';
