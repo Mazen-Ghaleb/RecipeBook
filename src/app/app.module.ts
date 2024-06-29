19a777e27fa2a0864d3a958fcded1f054de8d76e
Mazen <mazenghaleb@outlook.com>
2024-06-29 15:49:00 +0300

    Added service worker for offline capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
-import { CoreModule } from './core.module';
-import { SharedModule } from './shared/shared.module';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { environment } from '../environments/environment';
 
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import * as fromApp from './store/app.reducer';
@@ -31,6 +32,10 @@ import { RecipeEffects } from './recipes/store/recipe.effects';
     EffectsModule.forRoot([AuthEffects, RecipeEffects]),
     environment.production ? [] : StoreDevtoolsModule.instrument(),
     environment.production ? [] : StoreRouterConnectingModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [provideHttpClient(withInterceptorsFromDi())],
 })
